5e41e4ddbd5315a562a043818bffd50cadafb91a
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-04 23:43:29 +0000

    got pages working

--- express_server.js
+++ express_server.js
@@ -1,20 +1,20 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -23,4 +23,18 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let shortUrlKey = req.params.shortURL;
+  let longUrlValue = urlDatabase[shortUrlKey];
+  console.log(`this is shortUrlId ${shortUrlKey}`);
+  console.log(`this is urlD ${longUrlValue}`);
+  let templateVars = { shortURL: shortUrlKey , longURL: longUrlValue };
+  res.render("urls_show", templateVars);
 });
