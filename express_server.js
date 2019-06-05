354e87409745daaa51f83c6d4037d7c4278156bc
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-05 02:18:27 +0000

    redirect is working

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -26,13 +29,52 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let shortUrlKey = req.params.shortURL;
-  let longUrlValue = urlDatabase[shortUrlKey];
-  let templateVars = { shortURL: shortUrlKey , longURL: longUrlValue };
+  const shortUrlKey = req.params.shortURL;
+  const longUrlValue = urlDatabase[shortUrlKey];
+  const templateVars = { shortURL: shortUrlKey , longURL: longUrlValue };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortUrlKey = req.params.shortURL;
+  const longURL = urlDatabase[shortUrlKey];
+  res.redirect(longURL);
+});
+
+
+app.post("/urls", (req, res) => {
+  //console.log(req.body);
+  const enteredURL = {};
+  const longURL = req.body.longURL;
+
+  //generates random string 
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  console.log(`this urldatabase ${JSON.stringify(urlDatabase)}`);
+  //urlDatabase["shortURL"] = longURL;
+  //console.log(`this is longURL ${longURL}`);
+  //console.log(`this is shortURL ${shortURL}`);
+  res.send("Ok");        
+});
+
+
+//Generates random string 
+const generateRandomString = () => {
+  const result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = characters.length;
+  for ( var i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * length));
+  }
+  return result;
+};
+
