8a2db82c174b74241613df7502606ab63c5cb991
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-07 20:57:58 +0000

    hashed pw is working

--- express_server.js
+++ express_server.js
@@ -15,6 +15,11 @@ app.set("view engine", "ejs");
 // };
 
 
+// BCCRYPT 
+// *******************************************************
+const bcrypt = require('bcrypt');
+
+
 // URL DATABASES
 // *******************************************************
 const urlDatabase = {
@@ -41,6 +46,11 @@ const users = {
     id: "test",
     email: "test@test.com",
     password: "123"
+  },
+  "1va9Bj": {
+    id: '1va9Bj',
+    password: bcrypt.hashSync("123", 10),
+    email: 'steve@steve.com'
   }
 };
 
@@ -100,7 +110,18 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  console.log("this is password, ", password);
+
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  console.log("this is hashed password, ", hashedPassword);
+
+
+
   const userId = generateRandomString();
+
   res.cookie("user_id", userId);
 
   for (let key in users) {
@@ -121,10 +142,13 @@ app.post('/register', (req, res) => {
   } else {
     users[userId] = {};
     users[userId].id = userId;
-    users[userId].password = password;
+    users[userId].password = hashedPassword;
     users[userId].email = email;
     res.redirect(`/urls`);
   }
+
+  console.log("this is users, ", users);
+
 });
 
 
@@ -136,30 +160,42 @@ app.get('/login', (req, res) => {
 
 
 app.post('/login', (req, res) => {
+
   const email = req.body.email;
+  console.log(email);
+
   const password = req.body.password;
 
-  console.log("this is email: ", email);
-  console.log("this is password: ", password);
+  let pwChecker = false;
 
-  //Searches existing users for matching emails and sets cookie
+  let id;
+
   for (let key in users) {
+    console.log(key);
+    console.log(users[key].email);
+
     if (users[key].email === email) {
-      console.log(key);
-      res.cookie("user_id", users[key].id);
+
+      let hashedPassword = users[key].password;
+      console.log("this is checker, ", bcrypt.compareSync(password, hashedPassword));
+      pwChecker = bcrypt.compareSync(password, hashedPassword);
+      id = key;
     }
   }
 
-  //Checks password credentials
-  for (let key in users) {
-    if (users[key].email === email && users[key].password === password) {
-      res.redirect("/urls");
-    }
+
+
+  // //SUCCESSFUL LOGIN
+  if (pwChecker) {
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+  } else {
+    res.sendStatus(400);
   }
 
 
   //Failed login
-  res.sendStatus(400);
+
 
 });
 
@@ -271,7 +307,7 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: longUrlValue,
     user_id: req.cookies["user_id"],
 
-    //			MUST CHANGE!!!! ONLY FOR TESTING!!!!!
+    //      MUST CHANGE!!!! ONLY FOR TESTING!!!!!
     // ****************************************************************** 
     email: "test",
     // ***************************************************************** 
